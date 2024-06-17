09fcefdba2adf1081dd0c3e63f73b617b4a7a1ed
ilyas lokhand <ilokhandwala786@gmail.com>
2024-06-17 13:54:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function OpenPopup() {
+  popup.classList.add("open-popup");
+}
+
+function ClosePopup() {
+  popup.classList.remove("open-popup");
+}
